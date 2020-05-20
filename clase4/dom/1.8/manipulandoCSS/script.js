0b28e2d5c4bede8fa5d247c89ccfe07f213b1c59
Kezgan <lautaromcasella@gmail.com>
2020-05-20 05:26:54 -0300

    Actualizado e incompleto

---
+++ clase4/dom/1.8/manipulandoCSS/script.js
@@ -0,0 +1,5 @@
+var $div = document.querySelector("div")
+
+$div.classList.width = "1000px"
+$div.classList.height = "500px"
+$div.classList.backgroundColor = "blue"
---
+++ clase4/dom/1.8/shuffle/script.js
@@ -0,0 +1,7 @@
+var divSource = document.getElementById("source").textContent
+var swap1 = document.getElementById("swap1").textContent;
+var swap2 = document.getElementById("swap2").textContent;
+
+document.getElementById("destination").textContent = divSource
+document.getElementById("swap1").textContent = swap2;
+document.getElementById("swap2").textContent = swap1;
